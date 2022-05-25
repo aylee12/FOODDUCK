2253e2a5ba3754a81743562c8b8e98fedc37bed1
aylee12 <aylee5117@gmail.com>
2022-05-25 23:24:09 +0900

    js 상품 데이터 받아오기, 장바구니 페이지로 데이터 넘겨주기 뼈대 완료

--- src/views/product-detail/product-detail.js
+++ src/views/product-detail/product-detail.js
@@ -1,13 +1,19 @@
 window.onload = function() {
-  // 제품 정보 가져오기
-  fetch('api 주소')
-    .then(res => res.json())
-    .then(res => {
-      // data를 응답 받은 후의 로직
-
-      document.getElementById("").innerText = res;
-
-    });
+  // 제품 데이터 가져오기
+  // fetch('api 주소')
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     // data를 응답 받은 후의 로직
+  //     document.getElementById("product_img").innerText = res;
+  //     document.getElementById("product_name").innerText = res;
+  //     document.getElementById("product_price").innerText = res;
+  //     document.getElementById("product_sales_unit").innerText = res;
+  //     document.getElementById("product_volume").innerText = res;
+  //     document.getElementById("prodcut_origin").innerText = res;
+  //     document.getElementById("product_description").innerText = res;
+  //     document.getElementById("product_detail_information").innerText = res;
+  //     document.getElementById("product_review").innerText = res;
+  //   });
 
   const org_elem = document.getElementById("product_price");
   let org = parseInt(org_elem.innerText);
@@ -28,7 +34,7 @@ for(var i = 0; i < aTags.length; i ++) {
 }
 
 function count(type) {
-  const n_elem = document.getElementById("n");
+  const n_elem = document.getElementById("quantity");
   let n = parseInt(n_elem.value);
   
   const org_elem = document.getElementById("product_price");
@@ -43,4 +49,18 @@ function count(type) {
   n_elem.value = n;
   
   document.getElementById("sum").innerText = org * n;
+}
+
+function addToCart() {
+  // 사진, 이름, 가격, 수량, 총 가격
+  const img = document.getElementById("product_img").innerText;
+  const name = document.getElementById("product_name").innerText;
+  const price = document.getElementById("product_price").innerText;
+  const quantity = document.getElementById("quantity").innerText;
+  const total_price = document.getElementById("sum").innerText;
+
+  const product = {img: img, name: name, price: price, quantity: quantity, total_price: total_price};
+
+  localStorage.setItem('cart', JSON.stringify(product));
+  alert("장바구니에 추가되었습니다.");
 }
