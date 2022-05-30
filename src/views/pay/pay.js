49895ab746b226a7811f3d097daf1583dedea0b3
hovelopin <kimhojin3714@naver.com>
2022-05-30 14:58:16 +0900

    임시 커밋

--- src/views/pay/pay.js
+++ src/views/pay/pay.js
@@ -1,4 +1,5 @@
 const cartPriceSum = document.querySelector(".cart-price-sum");
+const payButton = document.querySelector("#payButton");
 
 // 주문 가격 총 계산하기 
 const paySumPriceLoaded = () => {
@@ -7,9 +8,18 @@ const paySumPriceLoaded = () => {
   cartPriceSum.innerHTML = sumPrice;
 }
 
+// 구매하기를 누를시에 Storage를 전부 비워주고 홈으로 이동시킨다.
+const handlePayBtn = () => {
+  // 요청을 하면 지워주고 
+  localStorage.removeItem("cart");
+  window.location.href = '/';
+  alert("결제가 완료되었습니다.");
+}
+
 // 실행 순서 정리 
 const init = () => {
   paySumPriceLoaded();
+  payButton.addEventListener("click" , handlePayBtn);
 }
 
 init();
