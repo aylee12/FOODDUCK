b4335a530b1c67ca0ea8883eccc72b237cae5058
hovelopin <kimhojin3714@naver.com>
2022-06-03 15:21:10 +0900

    fix : api 데이터 오류

--- src/views/user-info-cart/user-info-cart.js
+++ src/views/user-info-cart/user-info-cart.js
@@ -1,4 +1,29 @@
+import * as Api from '/api.js';
+
 const userCartCancel = document.querySelectorAll(".user_cart_cancel");
+const dbCartList = document.querySelector(".db_cart_list_container");
+
+// 즉시실행함수
+(async() => {
+  const user = await Api.get("/api/getUserInfo");
+  console.log(user);
+  const orderList = await Api.get(`/api/orderList`,`?userId=${user.userId}`);
+  console.log(orderList);
+  orderList.forEach(items =>{
+    const {status ,createdAt} = items;
+
+    dbCartList.innerHTML += 
+    `
+    <div class="db_cart_list_items1">${createdAt.split("T")[0]}</div>
+    <div class="db_cart_list_items2">아이보리 니트 1개</div>
+    <div class="db_cart_list_items3">${status}</div>
+    <div class="db_cart_list_items4">
+      <button type="button" class="user_cart_cancel">주문 취소</button>
+    </div>
+    `
+  })
+  
+})();
 
 // 삭제버튼을 누르면 데이터를 삭제해주기 
 userCartCancel.forEach(cancleBtn => {
