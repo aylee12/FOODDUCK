5a3f987789355792acdcbf16adaeb8eac437c02a
hovelopin <kimhojin3714@naver.com>
2022-06-03 20:56:13 +0900

    임시 커밋

--- src/views/user-info-cart/user-info-cart.js
+++ src/views/user-info-cart/user-info-cart.js
@@ -32,13 +32,14 @@ const dbCartList = document.querySelector(".db_cart_list_container");
     `
   });
 
-  const userCartCancel = document.querySelectorAll(".user_cart_cancel");
-  // 삭제버튼을 누르면 데이터를 삭제해주기 
-  userCartCancel.forEach(cancleBtn => {
-    cancleBtn.addEventListener("click" , () => {
-      console.log("들어와봐");
-      cancleBtn.parentElement.parentElement.remove();
-    });
-  });
 })();
 
+const userCartCancel = document.querySelectorAll(".user_cart_cancel");
+console.log(userCartCancel);
+  // 삭제버튼을 누르면 데이터를 삭제해주기 
+userCartCancel.forEach(cancleBtn => {
+  cancleBtn.addEventListener("click" , () => {
+    console.log("들어와봐");
+    cancleBtn.parentElement.parentElement.remove();
+  });
+});
